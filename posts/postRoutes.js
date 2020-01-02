f8e991cbaed4047f8116ad8598d55750275b836e
Heather Nuffer <zzzuzz@gmail.com>
2020-01-02 13:29:30 -0700

    Routes to api/posts/:id completed

--- posts/postRoutes.js
+++ posts/postRoutes.js
@@ -20,9 +20,40 @@ router.get('/', (req, res) => {
         })
         .catch(err => {
             console.log(err);
-            res.status(500).json({ error: "The posts information could not be retrieved." });
+            res.status(500).json({ error: "The posts' information could not be retrieved." });
         });
 })
+
+/*
+When the client makes a GET request to /api/posts/:id:
+
+    If the post with the specified id is not found:
+        return HTTP status code 404 (Not Found).
+        return the following JSON object: { message: "The post with the specified ID does not exist." }.
+
+    If there's an error in retrieving the post from the database:
+        cancel the request.
+        respond with HTTP status code 500.
+        return the following JSON object: { error: "The post information could not be retrieved." }.
+
+*/
+
+router.get(`/:id`, (req, res) => {
+    const id = req.params.id;
+    db.findById(id)
+        .then(blogPost => {
+            console.log(blogPost);
+            if (blogPost.length > 0) {
+                res.status(201).json(blogPost);
+            } else {
+                res.status(404).json({ message: "The post with the specified ID does not exist." });
+            }
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({ error: "The post information could not be retrieved." });
+        })
+});
 
 
 
