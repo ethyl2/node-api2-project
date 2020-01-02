d5abb56b39508a7d5483d2a20a4cfe81e1f896d7
Heather Nuffer <zzzuzz@gmail.com>
2020-01-02 13:19:34 -0700

    Route to api/posts completed

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const server = express();
+server.use(express.json());
 
-const postRoutes = require('./posts/postRoutes')
+const postRoutes = require('./posts/postRoutes');
 
-server.use('/posts', postRoutes);
+server.use('/api/posts', postRoutes);
+server.get('/', (req, res) => {
+    res.send('Welcome to the blog');
+});
+
 
 server.listen(9000, ()=> console.log('server up and running on port 9000'));
 
--- posts/postRoutes.js
+++ posts/postRoutes.js
@@ -1,8 +1,34 @@
 const express = require('express');
 const router = express.Router();
+const db = require('../data/db.js');
+
+/* 
+When the client makes a GET request to /api/posts:
+
+    If there's an error in retrieving the posts from the database:
+        cancel the request.
+        respond with HTTP status code 500.
+        return the following JSON object: { error: "The posts information could not be retrieved." }.
+
+*/
 
 router.get('/', (req, res) => {
-    res.status(200).json({"message": "Welcome to posts"})
+    db.find()
+        .then(posts => {
+            console.log(posts);
+            res.status(201).json(posts);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({ error: "The posts information could not be retrieved." });
+        });
 })
+
+
+
+
+
+
+
 
 module.exports = router;
